cad9e0409d526b5fbd7ce27dee297a5479972a9c
Adji25 <sayakadafi@gmail.com>
2022-03-23 02:36:10 -0700

    menambahkan pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya adji muhammad kadafi
+        saya tinggal didaerah ulujami
+        olahraga yang saya sukai adalah
+        futsal";
+    }
+}
+
